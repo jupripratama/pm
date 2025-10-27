6e7d12346670d6fd0d6d5795acce09a4a9814ce1
jupri eka pratama <jupri.eka@mkncorp.com>
2025-10-27 14:05:10 +0800

    Fix CORS

--- Program.cs
+++ Program.cs
@@ -119,7 +119,9 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
             "https://pmfrontend.vercel.app",
-            "http://localhost:3000" // untuk testing lokal
+            "http://localhost:3000", // untuk testing lokal
+            "http://localhost:5173", // Vite default port
+            "https://pmfrontend-git-*-jupripratamas-projects.vercel.app"
         )
         .AllowAnyHeader()
         .AllowAnyMethod()

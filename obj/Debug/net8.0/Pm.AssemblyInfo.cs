6e53bffeeb827337363a9288019ea63977bedba7
jupri eka pratama <jupri.eka@mkncorp.com>
2025-11-04 09:55:46 +0800

    role

--- obj/Debug/net8.0/Pm.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pm.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+895a5210b1e0cd018ca8d479794c64b100bb3d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bd762d6c70b62e7f3ffa9f139b99827dbad1f7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
